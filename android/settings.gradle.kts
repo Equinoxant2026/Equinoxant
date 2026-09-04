db2a521790709e75509dffff2f1537b0d5e4d8ff
Equinoxant2026 <2cSaziso@gmail.com>
2026-09-05 00:49:42 +0200

    Refactor flutterSdkPath initialization
    Refactor flutterSdkPath initialization to use let for better readability and error message.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,11 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
+    val flutterSdkPath = {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties or not set in HOME." }
+        return@let flutterSdkPath
+    }()
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -21,7 +20,6 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-}
 }
 
 include(":app")
