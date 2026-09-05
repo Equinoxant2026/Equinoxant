a3ff7d83fcc4e3dee9347d57e4a7674bf764d15d
Equinoxant2026 <2cSaziso@gmail.com>
2026-09-05 16:41:32 +0200

    Modify build.gradle.kts for new app configuration
    Updated application namespace and application ID to 'com.example.my_first_app'. Changed compile and target Java version to 17. Set NDK version to '30.0.16138531' and minimum SDK version to 23.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,25 +5,18 @@ plugins {
 }
 
 android {
-    namespace = "com.example.equinoxant"
+    namespace = "com.example.my_first_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30.0.16138531"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
-        applicationId = "com.example.equinoxant"
-        minSdk = flutter.minSdkVersion
+        applicationId = "com.example.my_first_app"
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = 1
         versionName = "1.0.0"
